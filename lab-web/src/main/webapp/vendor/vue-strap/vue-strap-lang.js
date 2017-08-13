601a9fd7565c096ca5cefa912330c3ef03876198
ajayhao <ajay.hao@gmail.com>
2017-08-13 11:53:05 +0800

    weekday排错了

--- lab-web/src/main/webapp/vendor/vue-strap/vue-strap-lang.js
+++ lab-web/src/main/webapp/vendor/vue-strap/vue-strap-lang.js
@@ -86,7 +86,7 @@ ru: {
 },
 
 zh: {
-  daysOfWeek: ['一', '二', '三', '四', '五', '六', '日'],
+  daysOfWeek: ['日','一', '二', '三', '四', '五', '六'],
   limit: 'Limit reached ({{limit}} items max).',
   loading: '拼命加载中...',
   minLength: '最小长度',
